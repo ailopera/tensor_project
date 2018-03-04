88d6d57e4c58b798c7f93b66dbd7ebd0dcc2dacf
ailopera <lopera.martinez.ana.isabel@gmail.com>
2018-03-04 16:46:59 +0000

    Correcciones finales sobre el script

--- data/aggregateData.py
+++ data/aggregateData.py
@@ -33,14 +33,14 @@ if __name__ == "__main__":
     outputBodyFile = outputPath + bodiesPrefixPath + "_clean.csv"
     outputStanceFile = outputPath + stancesPrefixPath + "_clean.csv"
     
-    # print(">> Limpiando fichero de bodies ", inputBodyFile, " y generando fichero ", outputBodyFile)
-    # cleanData.cleanTextData(False,inputBodyFile,outputBodyFile,True)
-    # print(">> Limpiando fichero de bodies ", inputStanceFile, " y generando fichero ", outputStanceFile)
-    # cleanData.cleanTextData(True,inputStanceFile,outputStanceFile,True)
+    print(">> Limpiando fichero de bodies ", inputBodyFile, " y generando fichero ", outputBodyFile)
+    cleanData.cleanTextData(False,inputBodyFile,outputBodyFile,False)
+    print(">> Limpiando fichero de bodies ", inputStanceFile, " y generando fichero ", outputStanceFile)
+    cleanData.cleanTextData(True,inputStanceFile,outputStanceFile,False)
 
     # Abrimos el fichero de stances y lo vamos recorriendo para hacer el agregado de los datos
     cleanedStanceData = pd.read_csv(outputStanceFile, header=0,delimiter=",", quoting=1)
-    cleanedBodyData = pd.read_csv(outputBodyFile, header=0,delimiter=",", quoting=1)
+    cleanedBodyData = pd.read_csv(outputBodyFile, header=0,delimiter=",", quoting=1, index_col='Body ID')
 
     # Creamos un indice para poder buscar en base a el mas adelante
     #cleanedBodyData.set_index("Body ID")
@@ -50,32 +50,36 @@ if __name__ == "__main__":
     # bodyFileDict = loadBodyDict(cleanedBodyData)
     print(">> Escribiendo resultados en ", aggregatedFile)
     with open(aggregatedFile, 'w') as aggregatedData:
-        fieldnames = ["Headline","ArticleBody", "Stance", "BodyIDS","BodyIDB"]
+        fieldnames = ["Headline","ArticleBody", "Stance", "BodyIDS"]
         
         writer = csv.DictWriter(aggregatedData, fieldnames=fieldnames)
         writer.writeheader()
-
+        i = 0
         for index, line in cleanedStanceData.iterrows():
             # Buscamos el cuerpo asociado
             bodyId = line["Body ID"]
             # associatedBody = cleanedBodyData.loc[cleanedBodyData['Body ID'] == bodyId, 'Body ID']
-            #associatedBody = cleanedBodyData.loc[bodyId]
-            associatedBody = cleanedBodyData.loc[cleanedBodyData['Body ID'] == bodyId]
-            print(">>> type(associatedBody): ", type(associatedBody))
+            associatedBody = cleanedBodyData.loc[bodyId]
+            # associatedBody = cleanedBodyData.loc[cleanedBodyData['Body ID'] == bodyId]
+            #associatedBodyId = cleanedBodyData.loc[cleanedBodyData['Body ID'] == bodyId, 'Body ID']
+            #print(">>> type(associatedBody): ", type(associatedBody))
             # if len(associatedBody) == 0:
             #     print(">> ERROR: He encontrado ", len(associatedBody), " elementos")
             #     print(">> Associated Body: ", associatedBody)
             # else: 
-            print(">> Associated Body: ", associatedBody)
+            
             aggregatedLine = {
                 "Headline": line["Headline"],
-                "ArticleBody": associatedBody["articleBody"],
+                "ArticleBody": associatedBody.get('articleBody'),
                 "Stance": line["Stance"],
-                "BodyIDS": bodyId,
-                "BodyIDB": associatedBody["Body ID"],
+                "BodyIDS": bodyId
+                # "BodyIDB": associatedBody.get('Body ID'),
             }
             # Escribimos la línea en el fichero
             writer.writerow(aggregatedLine)
-            print(">> ROW: ", aggregatedLine)
-
-            print("-------------------------------------")
+            if i%1000 == 0:
+                print(">> Counter: ", i)
+                print(">> Associated Body: ", associatedBody)
+                print(">> ROW: ", aggregatedLine)
+                print("-------------------------------------")
+            i +=1
