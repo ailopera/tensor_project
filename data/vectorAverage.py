41af095c87a73e1f24634c555e6dbe2c6ae4c8b3
ailm245 <ana.lopera@alten.es>
2018-03-04 18:34:10 +0100

    Usamos un modelo para las stances y otro para los bodies

--- data/vectorAverage.py
+++ data/vectorAverage.py
@@ -55,7 +55,8 @@ def getAvgFeatureVecs(news, model, num_features):
 if __name__ == "__main__":
 	basePath = "./fnc-1-original/aggregatedDatasets"
 	num_features = 300
-	model_name = sys.argv[1]
+	stances_model = sys.argv[1]
+	bodies_model = sys.argv[2]
 	
 	#model = KeyedVectors.load_word2vec_format(model_name)
 	model = Word2Vec.load(model_name)
@@ -75,8 +76,8 @@ if __name__ == "__main__":
 		clean_train_headlines.append(word2VecModel.news_to_wordlist(headline,remove_stopwords=True))
 		clean_train_articleBodies.append(word2VecModel.news_to_wordlist(articleBody,remove_stopwords=True))
 
-	trainDataVecs = getAvgFeatureVecs(clean_train_headlines, model, num_features)
-	trainDataVecs = getAvgFeatureVecs(clean_train_, model, num_features)
+	trainDataVecs['headline'] = getAvgFeatureVecs(clean_train_headlines, stances_model, num_features)
+	trainDataVecs['articleBody'] = getAvgFeatureVecs(clean_train_, bodies_model, num_features)
 
 	# Hacemos lo mismo con los datos de test
 	print(">> Generating word2vec model and applying vector average for test data...")
@@ -92,8 +93,8 @@ if __name__ == "__main__":
 		clean_test_headlines.append(word2VecModel.news_to_wordlist(headline,remove_stopwords=True))
 		clean_test_articleBodies.append(word2VecModel.news_to_wordlist(articleBody,remove_stopwords=True))
 
-	testDataVecs["articleBody"] = getAvgFeatureVecs(clean_test_articleBodies, model, num_features)
-	testDataVecs["Headline"] = getAvgFeatureVecs(clean_test_headlines, model, num_features)
+	testDataVecs["articleBody"] = getAvgFeatureVecs(clean_test_articleBodies, stances_model, num_features)
+	testDataVecs["Headline"] = getAvgFeatureVecs(clean_test_headlines, bodies_model, num_features)
 
 
 	# Creamos un modelo de random fores con los datos de entrenamiento, usando 100 árboles
