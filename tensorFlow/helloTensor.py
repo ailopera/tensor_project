19169bbd1d7206090c0e788df80eca9835d94825
ailopera <lopera.martinez.ana.isabel@gmail.com>
2018-03-13 19:53:11 +0000

    pequeño fix

--- tensorFlow/helloTensor.py
+++ tensorFlow/helloTensor.py
@@ -6,8 +6,8 @@ y = tf.Variable(2, name="y")
 f = x*x*y + y + 2
 
 sess = tf.Session()
-sess.run(x.initialilizer)
-sess.run(y.initialilizer)
+sess.run(x.initializer)
+sess.run(y.initializer)
 result = sess.run(f)
 print(result)
 sess.close()
