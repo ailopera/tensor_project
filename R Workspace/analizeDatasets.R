96ee3f16ebbcbb8829796e9ac0c053ac04eddcdd
ailm245 <ana.lopera@alten.es>
2018-04-04 22:06:05 +0200

    Añado carpeta con las estadísticas sobre los datos de entrada

---
+++ R Workspace/analizeDatasets.R
@@ -0,0 +1,20 @@
+library(ggplot2)
+library(gridExtra)
+
+# Read in csv files
+dfTrain <- read.table("data/train_data_aggregated.csv", header = TRUE, sep = ",")
+dfTest <- read.table("data/test_data_aggregated.csv", header = TRUE, sep = ",")
+dfCompetition <- read.table("data/competition_data_aggregated.csv", header = TRUE, sep = ",")
+
+# head(df$Stance)
+
+# Barplots de conteo de clasificaciones
+plotTrain <- ggplot(data=dfTrain, aes(x=Stance)) +  geom_bar(stat="count", fill="steelblue") + labs(title="Clasificaciones en el conjunto de entrenamiento", 
+       x="Etiqueta", y="Frecuencia")
+
+plotTest <- ggplot(data=dfTest, aes(x=Stance)) +  geom_bar(stat="count", fill="steelblue") + labs(title="Clasificaciones en el conjunto de test", 
+       x="Etiqueta", y="Frecuencia")
+
+plotCompetition <- ggplot(data=dfCompetition, aes(x=Stance)) +  geom_bar(stat="count", fill="steelblue") + labs(title="Clasificaciones en el conjunto de validación", 
+                                                                                                  x="Etiqueta", y="Frecuencia")
+
