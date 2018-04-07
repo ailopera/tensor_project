77b3a010b6c1c38e4a4986d6b35cd6a7bb601759
ailopera <lopera.martinez.ana.isabel@gmail.com>
2018-04-07 10:42:30 +0000

    Pequeñas modificaciones

--- data/loadWord2VecModel.py
+++ data/loadWord2VecModel.py
@@ -1,10 +1,22 @@
 import gensim
 from gensim.models import Word2Vec
 import sys
+
+# Obtiene los términos más cercanos y los imprime
+def get_most_similar( word):
+    similars = model.most_similar(word)
+    for word in similars:
+        print("> ",word[0],": ", word[1]) 
+
 inputPath = sys.argv[1] 
-#model = gensim.models.Word2Vec.load("300features_15minwords_10contextALL")
-#model = gensim.models.Word2Vec.load(inputPath)
-model = gensim.models.KeyedVectors.load_word2vec_format(inputPath, binary=True)
+binModel = len(sys.argv) == 3 and sys.argv[2] == False
+print("BinModel: ", binModel, len(sys.argv), sys.argv[2])
+print(">>> DOESNT_MATCH: Obtiene el término disonante con respecto al resto")
+if not binModel:
+    #model = gensim.models.Word2Vec.load("300features_15minwords_10contextALL")
+    model = gensim.models.Word2Vec.load(inputPath)
+else:
+    model = gensim.models.KeyedVectors.load_word2vec_format(inputPath, binary=True)
 
 # Paso 0: Representación numerica de los datos
 # The word2Vec model trained consists on a feature vector for each word in the vocabulary,
@@ -14,50 +26,64 @@ print("Model shape (Tamaño del vocabulario, tamaño del vector de features): ",
 
 # Paso 1: Exploración inicial del modelo
 # The doesht_math function will try to deduce which word in a set is most dissimilar form the others
-print(">>> DOESNT_MATCH: Obtiene el término disonante con respecto al resto")
+print(">>> doesnt_match: Obtiene el término disonante con respecto al resto")
 #print("---------------------------------------------------------------------------------------------")
 print("> Man, woman, child, kitchen: ", model.doesnt_match("man woman child kitchen".split()))
-#print("---------------------------------------------------------------------------------------------")
+print("> Dog, snake, horse, glass: ", model.doesnt_match("dog snake horse glass".split()))
+
+print("---------------------------------------------------------------------------------------------")
 print("> france, england, germany, berlin: ", model.doesnt_match("france england germany berlin".split()))
-#print("---------------------------------------------------------------------------------------------")
 print("> Paris, berlin, london, austria: ",model.doesnt_match("paris berlin london austria".split()))
 
+print("---------------------------------------------------------------------------------------------")
+print("> Streetlight, car, semaphore, mountain: ",model.doesnt_match("streetlight car semaphore mountain".split()))
+print("> Forest, team, queue, person: ",model.doesnt_match("forest team queue person".split()))
 # print("#############################################################################################")
 print(" ")
+
+print(">>> most_similar_cosmul: Obtiene el término más cercano tras aplicar una operación de sustracción sobre los vectores")
+forest_similar = model.wv.most_similar_cosmul(positive=['forest', 'person'], negative=['tree'])
+molecule_similar = model.wv.most_similar_cosmul(positive=['molecule'], negative=['group'])
+polymer_similar = model.wv.most_similar_cosmul(positive=['polymer'], negative=['natural'])
+print("embedded[forest] + embedded[person] - embedded[group]: ", forest_similar)
+print("embedded[molecule] - embedded[group]: ", molecule_similar)
+print("embedded[polymer] - embedded[natural]: ", polymer_similar)
 # We can also use the most_similar function to get insight into the model's wordclusters
 print(">>> MOST SIMILAR QUESTIONS")
 # print("> Man: ",get_most_similar("man"))
 # print("--------------------------------------------------------------------------------------------------------")
-print("> Woman: ",get_most_similar("woman"))
+print("> Woman: ")
+get_most_similar("woman")
 print("--------------------------------------------------------------------------------------------------------")
-print("> Queen: ", get_most_similar("queen"))
-
+print("> Queen: ")
+get_most_similar("queen")
 print("--------------------------------------------------------------------------------------------------------")
-print("> Obama: ", get_most_similar("obama"))
+print("> Obama: ")
+get_most_similar("obama")
 #print("---------------------------------------------------------------------------------------------")
 #print("> Trump: ", model.most_similar("trump")) # Not in vocabulary
 
 print("--------------------------------------------------------------------------------------------------------")
 #print("> Salary: ", model.most_similar("salary")) # Not in vocabulary
-print("> War: ", get_most_similar("war"))
+print("> War: ")
+get_most_similar("war")
 print("--------------------------------------------------------------------------------------------------------")
-print("> Crisis: ", get_most_similar("crisis"))
+print("> Crisis: ")
+get_most_similar("crisis")
 print("--------------------------------------------------------------------------------------------------------")
 # model.most_similar("horrible") #Not in vocabulary
 # model.most_similar("universal") # Not in vocabulary
-print("> Global: ", get_most_similar("global"))
+print("> Global: ")
+get_most_similar("global")
 print("---------------------------------------------------------------------------------------------------------")
-print("> Love: ", get_most_similar("love"))
+print("> Love: ")
+get_most_similar("love")
 print("---------------------------------------------------------------------------------------------------------")
-print("> Money: ", get_most_similar("money"))
+print("> Money: ")
+get_most_similar("money")
 print("---------------------------------------------------------------------------------------------------------")
 
 #print("----------------------------------------")
-#print("Sample word model: ", model["love"])
+get_most_similar("money")
+print("---------------------------------------------------------------------------------------------------------")
 
-
-# Obtiene los términos más cercanos y los imprime
-def get_most_similar(model, word):
-    similars = model.most_similar(word)
-    for word in similar:
-        print("> ",word[0],": ", word[1]) 
