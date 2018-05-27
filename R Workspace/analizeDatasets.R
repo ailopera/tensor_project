a1c464a762a03e0d7bc16f05b73f09120692a8a0
ailopera <ana.lopera@alten.es>
2018-05-27 13:15:15 +0200

    código para generar gráfico de clasificaciones de la particion 3

--- R Workspace/analizeDatasets.R
+++ R Workspace/analizeDatasets.R
@@ -2,12 +2,21 @@ library(ggplot2)
 library(gridExtra)
 setwd("C:/Users/Operador/projects/tensor_project/R Workspace")
 # Read in csv files
+# Datasets originales
 #dfTrain <- read.table("data/train_data_aggregated.csv", header = TRUE, sep = ",")
 #dfTest <- read.table("data/test_data_aggregated.csv", header = TRUE, sep = ",")
 #dfCompetition <- read.table("data/competition_data_aggregated.csv", header = TRUE, sep = ",")
 
-dfTrain <- read.table("../data/fnc-1-original/finalDatasets/train_partition.csv", header = TRUE, sep = ",")
-dfTest <- read.table("../data/fnc-1-original/finalDatasets/test_partition.csv", header = TRUE, sep = ",")
+#Particionado 1
+#dfTrain <- read.table("../data/fnc-1-original/finalDatasets/train_partition.csv", header = TRUE, sep = ",")
+#dfTest <- read.table("../data/fnc-1-original/finalDatasets/test_partition_3.csv", header = TRUE, sep = ",")
+# Particionado 2
+#dfTrain <- read.table("../data/fnc-1-original/finalDatasets/train_partition_split.csv", header = TRUE, sep = ",")
+#dfTest <- read.table("../data/fnc-1-original/finalDatasets/test_partition_split.csv", header = TRUE, sep = ",")
+
+# Particionado 3
+dfTrain <- read.table("../data/fnc-1-original/finalDatasets/train_partition_split.csv", header = TRUE, sep = ",")
+dfTest <- read.table("../data/fnc-1-original/finalDatasets/train_partition_3.csv", header = TRUE, sep = ",")
 
 # head(df$Stance)
 
@@ -17,7 +26,7 @@ dfTest <- read.table("../data/fnc-1-original/finalDatasets/test_partition.csv", 
 plotTrain <- ggplot(data=dfTrain, aes(x=Stance)) +  geom_bar(stat="count", fill="steelblue") + labs(title="Clasificaciones en el conjunto de entrenamiento/validación", 
        x="Etiqueta", y="Frecuencia")+ stat_count( aes(y=..count../sum(..count..), label=..count..), geom="text")
 
-plotTest <- ggplot(data=dfTest, aes(x=Stance)) +  geom_bar(stat="count", fill="steelblue") + labs(title="Clasificaciones en el conjunto de test", 
+plotTest <- ggplot(data=dfTest, aes(x=Stance)) +  geom_bar(stat="count", fill="steelblue") + labs(title="Clasificaciones en el conjunto de test (Particionado 3)", 
        x="Etiqueta", y="Frecuencia") + stat_count( aes(y=..count../sum(..count..), label=..count..), geom="text")
 
 
