9f9c65686fe8fc2dae00a27ef8c48a6bcb6ee4fe
ailopera <lopera.martinez.ana.isabel@gmail.com>
2018-06-03 18:12:06 +0000

    Comentando generación de fichero de características

--- data/embeddingVector.py
+++ data/embeddingVector.py
@@ -136,11 +136,16 @@ def executeVectorFeaturing(word2vec_model, model_executed, binary, trainData=Non
     print("> Time spent on formatting training data: ", trainDataFormattingTime)
     
     # Escribimos en un csv la longitud de cada frase y cuerpo de noticia
-    print("clean train headlines example: ", clean_train_headlines[0])
-    print("clean train headlines example: ", clean_train_articleBodies[0])
-    writeTextStats(clean_train_headlines, "headline")
-    writeTextStats(clean_train_articleBodies)
-
+    #print("clean train headlines example: ", clean_train_headlines[0])
+    #print("clean train headlines example: ", clean_train_articleBodies[0])
+    #writeTextStats(clean_train_headlines, "headline")
+    #writeTextStats(clean_train_articleBodies)
+    
+    
+    # Limitamos el tamaño de los cuerpos de noticia
+    max_sentences = 100
+    max_words = 150
+    
     # Obtenemos los vectores de caracteristicas de cada frase, sustituyendo cada termino por su representacion de embedding
     # print(">> Getting feature vectors for train headlines...")
     # start = time.time()
