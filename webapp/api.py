5e54b909be2fc63ef27d8285b927765ac4c23d87
ailopera <lopera.martinez.ana.isabel@gmail.com>
2018-06-30 18:12:24 +0000

    Correcciones. It works!

--- webapp/api.py
+++ webapp/api.py
@@ -3,34 +3,40 @@ from flask import request, jsonify, render_template
 from flask_restful import Resource, Api 
 
 from json import dumps 
-
+#from data import model
 import core
 import gensim
 from gensim.models import Word2Vec, KeyedVectors
+
+
+from flask_cors import CORS
 
 # Ejecucion -- Modo desarrollo
 # $ export FLASK_APP=api.py
 # $ flask run --host=0.0.0.0
 
-
+model = None
 app = Flask('NewsClassifier')
 api = Api(app)
+cors = CORS(app, resources={r"/stances/*": {"origins": "*"}})
 
-print(">> Loading word2vec Model...")
 word2vec_model = '../../GoogleNews-vectors-negative300.bin'
+print(">> Loading word2vec Model...")
 model = gensim.models.KeyedVectors.load_word2vec_format(word2vec_model, binary=True)
 print(">> Done! ")
 
+
 # Manejadores de la api
 class Stances(Resource):
-    def post(self):
+    
+    def post(self):        
         if 'body' in request.form and 'headline' in request.form:
             body = request.form['body']
             headline = request.form['headline']
             stance = core.predictStance(headline, body, model)
             return jsonify({'stance': stance})
         else:
-            return jsonify({'error': 'You must provide both body and articleBody params'})
+            return jsonify({'error': 'You must provide both body and headline params'})
 
 # Recursos de la API
 api.add_resource(Stances,'/stances')
